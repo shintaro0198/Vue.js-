c8d2f51835ac3a7e7eedd7bba49cd452b9c073ba
qudi <shintarou0327@yahoo.co.jp>
2021-02-27 11:49:26 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App">
+    <div class="tab">
+      <input type="radio" id="menu1" value="1" v-model="isActive">
+      <label for="menu1">タブメニュー１</label>
+      <input type="radio" id="menu2" value="2" v-model="isActive">
+      <label for="menu2">タブメニュー２</label>
+      <input type="radio" id="menu3" value="3" v-model="isActive">
+      <label for="menu3">タブメニュー３</label>
+    </div>
+    <ul>
+      <li v-if="isActive==='1'">
+        コンテンツ１コンテンツ１コンテンツ１コンテンツ１コンテンツ１コンテンツ１コンテンツ１コンテンツ１コンテンツ１
+      </li>
+      <li v-else-if="isActive==='2'">
+        コンテンツ２コンテンツ２コンテンツ２コンテンツ２コンテンツ２コンテンツ２コンテンツ２コンテンツ２コンテンツ２
+      </li>
+      <li v-else-if="isActive==='3'">
+        コンテンツ３コンテンツ３コンテンツ３コンテンツ３コンテンツ３コンテンツ３コンテンツ３コンテンツ３コンテンツ３
+      </li>
+    </ul>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      isActive:'1'
+    }
   }
 }
 </script>
+<style scoped>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+label{
+  padding: 20px;
+  height: 25px;
+  line-height: 25px;
+}
+input :checked + label{
+  background-color: black;
+  color:white}
+ul{
+  padding: 20px;
+  border:gray solid 1px;
+  list-style: none;
 }
 </style>
